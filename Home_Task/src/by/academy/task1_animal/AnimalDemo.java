5b1f03d52b5ee7a6b09dc9518de04bc1b8047d5e
Danila Gomerov <danila.gomerov@gmail.com>
2021-07-09 02:20:29 +0300

    Task1 classAnimal

---
+++ Home_Task/src/by/academy/task1_animal/AnimalDemo.java
@@ -0,0 +1,18 @@
+package by.academy.task1_animal;
+
+public class AnimalDemo {
+
+	public static void main(String[] args) {
+	
+		Animal birds = new Animal();
+		birds.setAge(14);
+		birds.setNickname("Valera");
+		Animal beer = new Animal(12,"Vity");
+		Animal elephant = new Animal(3,"Mariya");
+		System.out.println(birds.getAge() + "???" + birds.getNickname());
+		System.out.println(beer.age + "&&&" + beer.nickname);
+		System.out.println(elephant);
+
+	}
+
+}
