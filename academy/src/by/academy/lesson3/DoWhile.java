b77d43a58882f30b6a8076b54151d069b6fa11c8
Gomerov Daniil <danila.gomerov@gmail.com>
2021-07-14 17:43:18 +0300

    Lesson3

---
+++ academy/src/by/academy/lesson3/DoWhile.java
@@ -0,0 +1,15 @@
+package by.academy.lesson3;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		int n = 0;
+		
+		do{
+		System.out.println("“ик "+n++);	
+		}while(n < 11); 
+		System.out.println("End");
+
+	}
+
+}
