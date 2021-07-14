fbdccb8eee8f4e28c211e1dc87c566a3f373a293
User <User@User-PC>
2021-07-14 21:28:20 +0300

    Homework0

---
+++ academy/src/by/academy/homework0/Animal.java
@@ -0,0 +1,40 @@
+package by.academy.homework0;
+
+public class Animal {
+
+	int age;
+	String nickname;
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	public Animal() {
+		
+	}
+	
+	public Animal(int age, String nickname) {
+		super();
+		this.age = age;
+		this.nickname = nickname;
+	}
+
+	@Override
+	public String toString() {
+		return this.nickname +" [age=" + age + ", nickname=" + nickname + "]";
+	}
+
+	
+}
