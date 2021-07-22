ac38064f41eb1343babdd0eca580033bc4ee7c1a
Gomerov Daniil <danila.gomerov@gmail.com>
2021-07-22 21:18:05 +0300

    Lesson6

--- academy/src/by/academy/classwork/lesson5/Bike.java
+++ academy/src/by/academy/classwork/lesson5/Bike.java
@@ -13,4 +13,20 @@ public class Bike extends Product {
 		}
 		
 	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
 }
--- academy/src/by/academy/classwork/lesson5/Phone.java
+++ academy/src/by/academy/classwork/lesson5/Phone.java
@@ -2,7 +2,36 @@ package by.academy.classwork.lesson5;
 
 public class Phone extends Product {
 	
-	String color;
+	private String color;
+	private String brend;
+	private String country;
 	
+	public String getCountry(String brend) {
+		
+		if(brend == "Apple") {
+			country = "USA";
+		}
+		if(brend == "Xiaomi") {
+			country = "China";
+		}
+		
+		return country;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getBrend() {
+		return brend;
+	}
+
+	public void setBrend(String brend) {
+		this.brend = brend;
+	}
 
 }
