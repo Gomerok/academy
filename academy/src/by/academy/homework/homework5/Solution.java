bbfe901f349c7f938882c3368e0494ee7705e69c
Gomerov Daniil <danila.gomerov@gmail.com>
2021-08-21 11:48:41 +0300

    TaskSolution(fix)

--- academy/src/by/academy/homework/homework5/Solution.java
+++ academy/src/by/academy/homework/homework5/Solution.java
@@ -4,33 +4,6 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Solution {
-	
-//	Дан указатель на последний элемент двусвязного списка(tail). 
-//	Head/tail может быть null, это говорит о том, что список пуст, возвращаем null. 
-//	Поменять порядок элементов в списке на обратный. Измените следующий(next) и 
-//	предыдущий (prev) указатели всех узлов, чтобы направление списка изменилось. 
-//	Вернуть список на последний элемент измененного(перевернутого) списка. 
-//	Описание:
-//	Закончите функцию ниже, которая изменит порядок списка на обратный. Функция принимает следующие параметры:
-//	head – указатель на последний элемент списка в DoublyLinkedList
-//	Формат ввода:
-//	Первая линия принимает integer t – количество элементов в списке
-//	Следующие n линий – представляют собой элементы двусвязного списка.
-//	Ограничения:
-//	0 <= n <= 1000
-//	0 <= DoubleLinkedListNode.data <= 1000
-//	Формат вывода:
-//	Должен вернуться указатель на последний элемент перевернутого списка.
-//	Пример ввода:
-//	4 
-//	1
-//	2
-//	3
-//	4
-//	Пример вывода:
-//	4 3 2 1
-	 
-
 
 	static class DoublyLinkedListNode {
 		public int data;
@@ -79,18 +52,27 @@ public class Solution {
 		}
 	}
 
-	// Complete the reverse function below.
+	public static DoublyLinkedListNode reverse(DoublyLinkedListNode headNode) {
+		if (headNode == null || headNode.next == null) {
+			return headNode;
+		}
 
-	/*
-	 * For your reference:
-	 *
-	 * DoublyLinkedList { int data; DoublyLinkedListNode next; DoublyLinkedListNode
-	 * prev; }
-	 *
-	 */
-	public static DoublyLinkedListNode reverse(DoublyLinkedList list) {
+		DoublyLinkedListNode currentNode = headNode;
+		DoublyLinkedListNode nextNode = null;
 
-		return null;
+		while (true) {
+			nextNode = currentNode.next;
+
+			currentNode.next = currentNode.prev;
+			currentNode.prev = nextNode;
+
+			if (nextNode == null) {
+				break;
+			}
+			currentNode = nextNode;
+		}
+
+		return currentNode;
 	}
 
 	private static final Scanner scanner = new Scanner(System.in);
@@ -108,7 +90,8 @@ public class Solution {
 
 			llist.insertNode(llistItem);
 		}
-		DoublyLinkedListNode llist1 = reverse(llist);
+
+		DoublyLinkedListNode llist1 = reverse(llist.head);
 
 		printDoublyLinkedList(llist1, " ");
 		scanner.close();
